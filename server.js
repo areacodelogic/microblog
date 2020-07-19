8126326932e59d6855db7e36932e6a4b9f87dbcf
timothy egart <areacodelogic@gmail.com>
2020-07-18 20:05:01 -0700

    ready to deploy

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-
-
 const app = require('./app');
-
-
-
-
-
-const PORT = process.env.PORT || 5000;
-
+const { PORT } = require('./config');
 
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
